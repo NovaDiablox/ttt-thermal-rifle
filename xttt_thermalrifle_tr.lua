4373609d92b4accfd4a1f3ad5f3065a032909146
Tora <49911006+NovaDiablox@users.noreply.github.com>
2024-11-15 19:30:41 +0300

    Added Turkish Localization

---
+++ xttt_thermalrifle_tr.lua
@@ -0,0 +1,4 @@
+local L = LANG.GetLanguageTableReference("tr")
+
+L["xtttthermalrifle_name"] = "Termal Tüfek"
+L["xtttthermalrifle_desc"] = "Termal Dürbünlü askerî tüfek, hedefleri sisin ardından bile vurgular."
